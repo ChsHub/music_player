0b6d73046f40e82f48a2c3ee616c4024a2bd7acf
ChsHub <christian1193@t-online.de>
2022-02-28 16:50:02 +0100

    Add example IPC; activity_main.xml: Add ListView for directory view; AndroidManifest.xml: Make service run in separate process; MainActivity.java: remove getCachedFile;

--- app/src/main/java/com/example/music_player/MainActivity.java
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
@@ -22,15 +21,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 
 import static android.util.Log.e;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Binding
+{
     public static final String CHANNEL_ID = "exampleServiceChannel";
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 0;
@@ -38,43 +34,35 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
-
-    public static File getCacheFile(Context context) {
-        String path = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.notification2).toString();
-        File cacheFile = new File(context.getCacheDir(), path);
-        try {
-            InputStream inputStream = context.getAssets().open(path);
-            try {
-                FileOutputStream outputStream = new FileOutputStream(cacheFile);
-                try {
-                    byte[] buf = new byte[1024];
-                    int len;
-                    while ((len = inputStream.read(buf)) > 0) {
-                        outputStream.write(buf, 0, len);
-                    }
-                } finally {
-                    outputStream.close();
-                }
-            } finally {
-                inputStream.close();
-            }
-        } catch (IOException e) {
-            e("MainActivity", "FILE NOT FOUND");
-        }
-        return cacheFile;
-    }
+    protected Intent playerIntent;
 
     /**
      * Checks if the app has permission to write to device storage.
      * If the app does not has permission then the user will be prompted to grant permissions.
      */
-    public void verifyStoragePermissions() {
+    public void verifyStoragePermissions()
+    {
         ActivityCompat.requestPermissions(
                 this,
                 PERMISSIONS_STORAGE,
                 REQUEST_EXTERNAL_STORAGE
         );
-        //}
+    }
+
+    /**
+     * Notification Channel for PlayerService
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    protected void createNotificationChannel()
+    {
+
+        Uri notificationSound = Uri.parse("android.resource://" + this.getPackageName() + "/" + R.raw.notification2);
+        NotificationChannel serviceChannel = new NotificationChannel(CHANNEL_ID, "Example Service Channel", NotificationManager.IMPORTANCE_HIGH);
+        AudioAttributes audioAttributes = serviceChannel.getAudioAttributes();
+
+        serviceChannel.setSound(notificationSound, audioAttributes); // Can only be set before createNotificationChannel
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(serviceChannel);
     }
 
     /**
@@ -84,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Logger logger = new Logger();
@@ -101,14 +90,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        //Create Notification Channel
-        Uri notificationSound = Uri.parse("android.resource://" + this.getPackageName() + "/" + R.raw.notification2);
-        NotificationChannel serviceChannel = new NotificationChannel(CHANNEL_ID, "Example Service Channel", NotificationManager.IMPORTANCE_HIGH);
-        AudioAttributes audioAttributes = serviceChannel.getAudioAttributes();
-
-        serviceChannel.setSound(notificationSound, audioAttributes); // Can only be set before createNotificationChannel
-        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.createNotificationChannel(serviceChannel);
+        createNotificationChannel();
     }
 
     /**
@@ -116,7 +98,8 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view
      */
-    public void startService(View view) {
+    public void startService(View view)
+    {
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
@@ -143,26 +126,36 @@ public class MainActivity extends AppCompatActivity {
         if (audioExtra.contains("document/raw:")) {
             audioExtra = audioExtra.replace("/document/raw:", "");
         }
+        // Start music service, with attached intent
         if (!audioExtra.equals("")) {
-            Intent serviceIntent = new Intent(this, PlayerService.class);
-            serviceIntent.putExtra("inputExtra", audioExtra); // Puts intent with audio path as extra
-            ContextCompat.startForegroundService(this, serviceIntent);
+            playerIntent = new Intent(this, PlayerService.class);
+            playerIntent.putExtra("inputExtra", audioExtra); // Puts intent with audio path as extra
+            ContextCompat.startForegroundService(this, playerIntent);
         }
     }
 
-    Uri handleSend(Intent intent) {
+    Uri handleSend(Intent intent)
+    {
         return intent.getParcelableExtra(Intent.EXTRA_STREAM);
     }
 
-    void handleSendMultiple(Intent intent) {
+    void handleSendMultiple(Intent intent)
+    {
         ArrayList<Uri> audioURIs = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
         if (audioURIs != null) {
-            // TODO Update UI to reflect multiple images being shared
+            // TODO Update UI to reflect multiple files
         }
     }
 
-    public void stopService(View v) {
-        Intent serviceIntent = new Intent(this, PlayerService.class);
-        stopService(serviceIntent);
+    /**
+     * TODO On Stop Button click, pause the song
+     *
+     * @param v
+     */
+    public void stopService(View v)
+    {
     }
+
+
+
 }
--- app/src/main/java/com/example/music_player/PlayerService.java
+++ app/src/main/java/com/example/music_player/PlayerService.java
@@ -2,14 +2,13 @@ package com.example.music_player;
 
 import android.app.Notification;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
-import android.os.IBinder;
+import android.os.Handler;
+import android.os.Message;
 
-import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import java.io.File;
@@ -17,15 +16,35 @@ import java.io.File;
 import static android.util.Log.e;
 import static com.example.music_player.MainActivity.CHANNEL_ID;
 
-public class PlayerService extends Service {
+/***
+ * Service that runs the music player.
+ * Should run in own thread since it's CPU heavy:
+ * https://developer.android.com/reference/android/app/Service
+ */
+public class PlayerService extends MessengerService
+{
+    /**
+     * Handler of incoming messages from clients.
+     */
+    class IncomingHandler extends Handler
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            mValue = msg.arg1;
+        }
+    }
+
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
     }
 
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
         String input = intent.getStringExtra("inputExtra");
         Intent notificationIntent = new Intent(this, MainActivity.class);
         // Open activity when clicking on notification
@@ -35,7 +54,7 @@ public class PlayerService extends Service {
             // Get sound URI and Attributes for channel
 
             Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setContentTitle("@string/button_start") //TODO set string
+                    .setContentTitle("Now playing: " + input) //TODO set string
                     .setContentText(input)
                     .setSmallIcon(R.drawable.ic_home_black_24dp)
                     .setContentIntent(pendingIntent)
@@ -50,14 +69,21 @@ public class PlayerService extends Service {
 
         startPlayer(input);
 
-        return START_NOT_STICKY;
+        return START_STICKY; // START_STICKY for explicit start and stop of Service https://developer.android.com/reference/android/app/Service#START_STICKY
+    }
+
+    @Override
+    public void showNotification()
+    {
+
     }
 
     /***
      * Play audio file
      * @param path Audio file path
      */
-    public void startPlayer(String path) {
+    protected void startPlayer(String path)
+    {
         // Creating file is needed for MediaPlayer
         File file = new File(path);
         if (!file.exists()) {
@@ -75,17 +101,5 @@ public class PlayerService extends Service {
         //player.prepareAsync();
         player.setVolume(50, 50);
         player.start();
-
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
     }
 }
